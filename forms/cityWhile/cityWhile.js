987fafe6e5dd0b6983d166e26a22237d13954845
chc72331 <chc72331@creighton.edu>
2021-03-14 16:42:38 -0500

    Update cityWhile.js

--- forms/cityWhile/cityWhile.js
+++ forms/cityWhile/cityWhile.js
@@ -1,4 +1,21 @@
 // Create some variables
 
 let cities = []
-let 
+let cityBoolean = false
+let userCity = ""
+
+while (cityBoolean == false) {
+  userCity = prompt('Do you have any cities to enter? (Enter or "n" if you\'re done)')
+  if (userCity == "n") {
+    cityBoolean = true
+  } else {
+    cities.push(userCity)
+  }
+}  
+
+let i = 0
+
+while (i < cities.length) {
+  console.log(cities[i].toLowerCase())
+  i++;
+}

e3fa2d8c8a0d02ae078bba24e69c1871f4b824ce
chc72331 <chc72331@creighton.edu>
2021-03-14 23:21:21 -0500

    Update ECnestedLoops.js

--- forms/ECnestedLoops/ECnestedLoops.js
+++ forms/ECnestedLoops/ECnestedLoops.js
@@ -1,3 +1,4 @@
+/*
 let columns = prompt('How many asterisks do you want per line (1 to 10)')
 let rows = prompt('How many lines do you want (1 to 10)?')
 let randomChar = ''
@@ -9,3 +10,4 @@ for (i = 0; i < rows; i++) {
   randomChar += "<br/>"
 }
 document.writeln(randomChar)
+*/

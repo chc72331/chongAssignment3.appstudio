ce6360e3607315adca7a989c7595fe95d479e955
chc72331 <chc72331@creighton.edu>
2021-03-14 21:24:48 -0500

    Update functionCompare.js

--- forms/functionCompare/functionCompare.js
+++ forms/functionCompare/functionCompare.js
@@ -1,11 +1,13 @@
-function comparePassword(pass1, pass2) {
-  if (pass1 == pass2) {
-    console.log("The passwords are the same.")
-  } else {
-    console.log("The passwords are not the same.")
-  }
+function compare(pass1, pass2) {
+  return pass1.localeCompare(pass2) // This will return a -1, 0 or 1
 }
-
+  
 let password = prompt("Enter your password")
 let confirm = prompt("Confirm your password")
-let passwordComparison = comparePassword(password, confirm)
+
+let binaryResult = compare(password, confirm)
+
+if (binaryResult == 0)
+  console.log('The passwords are the same.')
+else
+  console.log('The passwords are not the same.')
